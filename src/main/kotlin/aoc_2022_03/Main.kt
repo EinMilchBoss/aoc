f145e5cc957745d7e4fcd5d56b2a98de9a3882ab
EinMilchBoss <christian.benezuela@gmail.com>
2022-12-03 18:32:26 +0100

    Add 20221203.

---
+++ src/main/kotlin/aoc_2022_03/Main.kt
@@ -0,0 +1,41 @@
+package aoc_2022_03
+
+import test
+import java.io.File
+
+fun Char.priority(): Int =
+    if (this in 'a'..'z') this - 'a' + 1
+    else this - 'A' + 27
+
+fun String.splitInHalf(): Pair<String, String> =
+    (length / 2).let { halfSize ->
+        take(halfSize) to takeLast(halfSize)
+    }
+
+fun solveFirst(input: List<String>): String =
+    input.map(String::splitInHalf)
+        .map { (compartmentA, compartmentB) ->
+            compartmentA.first { it in compartmentB }
+        }
+        .sumOf(Char::priority)
+        .toString()
+
+fun solveSecond(input: List<String>): String =
+    input.chunked(3)
+        .map { (rucksackA, rucksackB, rucksackC) ->
+            rucksackA.first { it in rucksackB && it in rucksackC }
+        }
+        .sumOf(Char::priority)
+        .toString()
+
+fun main() {
+    val pathPrefix = "./src/main/kotlin/aoc_2022_03"
+
+    val exampleInput = File("$pathPrefix/example.txt").readLines()
+    println("First test: ${test(exampleInput, "157", ::solveFirst)}")
+    println("Second test: ${test(exampleInput, "70", ::solveSecond)}")
+
+    val input = File("$pathPrefix/input.txt").readLines()
+    println("First result: ${solveFirst(input)}")
+    println("Second result: ${solveSecond(input)}")
+}
