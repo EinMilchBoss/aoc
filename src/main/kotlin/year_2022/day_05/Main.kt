6431f34352bc8a25a8a799d9fe0e4429a6cea901
EinMilchBoss <christian.benezuela@gmail.com>
2022-12-05 11:37:34 +0100

    Refactor 20221205.

--- src/main/kotlin/year_2022/day_05/Main.kt
+++ src/main/kotlin/year_2022/day_05/Main.kt
@@ -4,88 +4,81 @@ import utils.pop
 import utils.test
 import java.io.File
 import java.util.*
+import javax.swing.SortOrder
 
 data class Instruction(val amount: Int, val from: Int, val to: Int)
 
-fun solveFirst(input: List<String>): String {
-    val inter = input.joinToString("\n")
-        .split("\n\n")
-        .let { (crateBlock, instructionBlock) ->
-            Pair(
-                crateBlock.split("\n")
-                    .dropLast(1)
-                    .map { line ->
-                        (1..33 step 4).mapNotNull { index ->
-                            line.getOrElse(index) { ' ' }
-                        }
-                    }
-                    .foldRight(List(9) { Stack<Char>() }) { chars, acc ->
-                        acc.also { stacks ->
-                            stacks.forEachIndexed { index, stack ->
-                                chars[index].let { if (!it.isWhitespace()) stack.push(it) }
-                            }
-                        }
-                    },
-                instructionBlock.split("\n")
-                    .map { line ->
-                        line.split(' ')
-                            .let { Instruction(it[1].toInt(), it[3].toInt(), it[5].toInt()) }
-                    }
-            )
+data class CrateStack(private val values: MutableList<Char>) {
+    fun peekOrNull(): Char? =
+        values.lastOrNull()
+
+    fun push(element: Char) =
+        values.add(element)
+
+    fun push(elements: List<Char>) =
+        values.addAll(elements)
+
+    fun pop(n: Int) =
+        values.pop(n)
+}
+
+fun String.parseCrates(): List<CrateStack> =
+    split("\n")
+        .dropLast(1)
+        .map { line ->
+            (1..33 step 4).mapNotNull { index ->
+                line.getOrElse(index) { ' ' }
+            }
         }
-        .let { (crates, instructions) ->
-            instructions.forEach { instruction ->
-                repeat(instruction.amount) {
-                    crates[instruction.from - 1].pop()
-                        .let { popped ->
-                            crates[instruction.to - 1].push(popped)
-                        }
+        .foldRight(List(9) { CrateStack(mutableListOf()) }) { chars, acc ->
+            acc.also { stacks ->
+                stacks.forEachIndexed { index, stack ->
+                    chars[index].let { if (!it.isWhitespace()) stack.push(it) }
                 }
             }
-            crates.map { it.lastOrNull()?.toString() ?: "" }
         }
 
-    return inter.joinToString("")
-}
+fun String.parseInstructions(): List<Instruction> =
+    split("\n")
+        .map { line ->
+            line.split(' ')
+                .let { Instruction(it[1].toInt(), it[3].toInt(), it[5].toInt()) }
+        }
 
-fun solveSecond(input: List<String>): String {
-    val inter = input.joinToString("\n")
+fun List<String>.parse(): Pair<List<CrateStack>, List<Instruction>> =
+    joinToString("\n")
         .split("\n\n")
         .let { (crateBlock, instructionBlock) ->
             Pair(
-                crateBlock.split("\n")
-                    .dropLast(1)
-                    .map { line ->
-                        (1..33 step 4).mapNotNull { index ->
-                            line.getOrElse(index) { ' ' }
-                        }
-                    }
-                    .foldRight(List(9) { mutableListOf<Char>() }) { chars, acc ->
-                        acc.also { lists ->
-                            lists.forEachIndexed { index, list ->
-                                chars[index].let { if (!it.isWhitespace()) list.add(it) }
-                            }
-                        }
-                    },
-                instructionBlock.split("\n")
-                    .map { line ->
-                        line.split(' ')
-                            .let { Instruction(it[1].toInt(), it[3].toInt(), it[5].toInt()) }
-                    }
+                crateBlock.parseCrates(),
+                instructionBlock.parseInstructions()
             )
         }
-        .let { (crates, instructions) ->
-            instructions.forEach { instruction ->
-                crates[instruction.from - 1].pop(instruction.amount)
-                    .let { removed ->
-                        crates[instruction.to - 1].addAll(removed)
-                    }
-            }
-            crates.map { it.lastOrNull()?.toString() ?: "" }
-        }
 
-    return inter.joinToString("")
-}
+fun List<String>.solve(logic: (Pair<List<CrateStack>, List<Instruction>>) -> List<String>): String =
+    parse().let(logic).joinToString("")
+
+fun solveFirst(input: List<String>): String =
+    input.solve { (crates, instructions) ->
+        instructions.forEach { instruction ->
+            crates[instruction.from - 1].pop(instruction.amount)
+                .let { popped ->
+                    crates[instruction.to - 1].push(popped.reversed())
+                }
+        }
+        crates.map { it.peekOrNull()?.toString() ?: "" }
+    }
+
+fun solveSecond(input: List<String>): String =
+    input.solve { (crates, instructions) ->
+        instructions.forEach { instruction ->
+            crates[instruction.from - 1].pop(instruction.amount)
+                .let { removed ->
+                    crates[instruction.to - 1].push(removed)
+                }
+        }
+        crates.map { it.peekOrNull()?.toString() ?: "" }
+    }
 
 fun main() {
     val pathPrefix = "./src/main/kotlin/year_2022/day_05"
