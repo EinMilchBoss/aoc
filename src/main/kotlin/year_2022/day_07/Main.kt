9901a7be4a2c67a9da762af1bc05bacc7527ff85
EinMilchBoss <christian.benezuela@gmail.com>
2022-12-07 12:28:37 +0100

    Further refactoring.

--- src/main/kotlin/year_2022/day_07/Main.kt
+++ src/main/kotlin/year_2022/day_07/Main.kt
@@ -3,45 +3,10 @@ package year_2022.day_07
 import utils.test
 import java.io.File
 
-fun String.isDirectory(): Boolean =
-    startsWith("dir")
-
-fun String.parseDirectory(parentDirectory: FileSystem.Directory): FileSystem.Directory =
-    FileSystem.Directory(split(" ").last(), parentDirectory)
-
-fun String.parseFile(): FileSystem.File =
-    split(" ").let { (size, name) ->
-        FileSystem.File(name, size.toInt())
-    }
-
-fun FileSystem.execute(dialogs: List<Dialog>): FileSystem {
-    tailrec fun iterate(currentDirectory: FileSystem.Directory, dialogIndex: Int) {
-        if (dialogIndex > dialogs.lastIndex) return
-
-        dialogs[dialogIndex].let { (command, output) ->
-            if (command.startsWith("ls")) {
-                currentDirectory.directories.addAll(
-                    output.filter { it.isDirectory() }
-                        .map { it.parseDirectory(currentDirectory) }
-                )
-                currentDirectory.files.addAll(
-                    output.filter { !it.isDirectory() }
-                        .map(String::parseFile)
-                )
-                return iterate(currentDirectory, dialogIndex + 1)
-            }
-            if (command.startsWith("cd")) {
-                return iterate(currentDirectory.changeDirectory(command.split(' ').last()), dialogIndex + 1)
-            }
-        }
-    }
-    return also { iterate(root, 0) }
-}
-
-data class Dialog(val command: String, val output: List<String>)
+data class Dialog(val input: String, val output: List<String>)
 
 data class FileSystem(val root: Directory = Directory("/", null)) {
-    fun findDirectories(predicate: (size: Int) -> Boolean): List<Directory> {
+    fun filterDirectories(predicate: (size: Int) -> Boolean): List<Directory> {
         fun iterate(currentDirectory: Directory): List<Directory> {
             return if (predicate(currentDirectory.size())) {
                 listOf(currentDirectory) + currentDirectory.directories.flatMap { directory -> iterate(directory) }
@@ -51,50 +16,97 @@ data class FileSystem(val root: Directory = Directory("/", null)) {
         return iterate(root)
     }
 
-    data class File(val name: String, val size: Int)
+    data class File(val name: String, val size: Int) {
+        companion object {
+            fun fromEntry(entry: String): File =
+                entry.split(" ").let { (size, name) ->
+                    File(name, size.toInt())
+                }
+        }
+    }
+
     data class Directory(
         val name: String,
-        val parentDirectory: Directory?,
+        val parent: Directory?,
         val files: MutableList<File> = mutableListOf(),
         val directories: MutableList<Directory> = mutableListOf()
     ) {
+        companion object {
+            fun fromEntry(entry: String, parentDirectory: Directory): Directory =
+                Directory(
+                    entry.split(" ").last(),
+                    parentDirectory
+                )
+
+            fun isEntryDirectory(entry: String): Boolean =
+                entry.startsWith("dir")
+        }
+
+        fun addElements(content: List<String>): Directory =
+            apply {
+                content.forEach { entry ->
+                    if (isEntryDirectory(entry)) directories.add(fromEntry(entry, this))
+                    else files.add(File.fromEntry(entry))
+                }
+            }
+
         fun changeDirectory(name: String): Directory =
             if (name != "..") directories.first { it.name == name }
-            else parentDirectory!!
+            else parent!!
 
         fun size(): Int =
             files.sumOf { it.size } + directories.sumOf { it.size() }
     }
 }
 
+fun FileSystem.explore(dialogs: List<Dialog>): FileSystem {
+    tailrec fun iterate(currentDirectory: FileSystem.Directory, dialogIndex: Int) {
+        if (dialogIndex > dialogs.lastIndex) return
+
+        dialogs[dialogIndex].let { (input, output) ->
+            if (input.startsWith("cd")) {
+                return iterate(
+                    currentDirectory.changeDirectory(input.split(' ').last()),
+                    dialogIndex + 1
+                )
+            }
+            if (input.startsWith("ls")) {
+                currentDirectory.addElements(output)
+                return iterate(currentDirectory, dialogIndex + 1)
+            }
+        }
+    }
+    return apply { iterate(root, 0) }
+}
+
 fun List<String>.parse(): List<Dialog> =
     joinToString("\n")
         .split("$")
-        .filterNot { it.isBlank() }
+        .filterNot(String::isBlank)
         .drop(1)
-        .map(String::trim)
         .map { dialog ->
-            dialog.split("\n")
-                .let { dialogLines ->
-                    Dialog(dialogLines.first(), dialogLines.drop(1))
+            dialog.trim()
+                .split("\n")
+                .run {
+                    Dialog(first(), drop(1))
                 }
         }
 
 fun solveFirst(input: List<String>): String {
-    val inter = FileSystem().execute(input.parse())
-        .findDirectories { it <= 100_000 }
+    val inter = FileSystem().explore(input.parse())
+        .filterDirectories { it <= 100_000 }
         .sumOf { it.size() }
 
     return inter.toString()
 }
 
 fun solveSecond(input: List<String>): String {
-    val fileSystem = FileSystem().execute(input.parse())
+    val fileSystem = FileSystem().explore(input.parse())
     val maxUsedSpace = 40_000_000
     val toBeDeleted = fileSystem.root.size() - maxUsedSpace
 
     val inter = fileSystem
-        .findDirectories { it >= toBeDeleted }
+        .filterDirectories { it >= toBeDeleted }
         .minOf { it.size() }
 
     return inter.toString()
