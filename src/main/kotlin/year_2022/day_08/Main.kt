fd5444e6e3a5e6f03746bc3fceae0c5c83268fb3
EinMilchBoss <christian.benezuela@gmail.com>
2022-12-09 10:51:16 +0100

    Refactor 20221208.

--- src/main/kotlin/year_2022/day_08/Main.kt
+++ src/main/kotlin/year_2022/day_08/Main.kt
@@ -5,103 +5,67 @@ import java.io.File
 
 data class Tree(val x: Int, val y: Int, val height: Int)
 
-typealias Grid = List<List<Tree>>
+typealias TreeGrid = List<List<Tree>>
 
-fun List<Tree>.visibleTrees(): List<Tree> {
-    fun iterate(index: Int, highest: Int, matches: List<Tree>): List<Tree> =
+fun TreeGrid.rowsToCols(): TreeGrid =
+    mapIndexed { y, line ->
+        List(line.size) { x -> this[x][y] }
+    }
+
+fun TreeGrid.scenicScore(tree: Tree): Int =
+    listOf(
+        this[tree.y].run { subList(tree.x + 1, size) },
+        this[tree.y].run { subList(0, tree.x).reversed() },
+        rowsToCols()[tree.x].run { subList(tree.y + 1, size) },
+        rowsToCols()[tree.x].run { subList(0, tree.y).reversed() },
+    ).map { it.visibleFromTreeHouse(tree).count() }
+        .reduce { acc, height -> acc * height}
+
+fun List<Tree>.visibleFromGround(): List<Tree> {
+    tailrec fun iterate(index: Int, highest: Int, matches: List<Tree>): List<Tree> =
         if (index > lastIndex) matches
-        else this[index].run {
-            if (height > highest) iterate(index + 1, height, matches + listOf(this))
+        else
+            if (this[index].height > highest) iterate(index + 1, this[index].height, matches + listOf(this[index]))
             else iterate(index + 1, highest, matches)
-        }
     return iterate(0, -1, emptyList())
 }
 
-fun List<String>.toGrid(): Grid =
+fun List<Tree>.visibleFromTreeHouse(house: Tree): List<Tree> {
+    tailrec fun iterate(index: Int, matches: List<Tree>): List<Tree> =
+        if (index > lastIndex) matches
+        else
+            if (this[index].height < house.height) iterate(index + 1, matches + listOf(this[index]))
+            else matches + listOf(this[index])
+    return iterate(0, emptyList())
+}
+
+fun List<String>.toGrid(): TreeGrid =
     mapIndexed { y, line ->
         line.toCharArray()
             .mapIndexed { x, _ -> Tree(x, y, this[y][x].digitToInt()) }
-    }
-
-fun Grid.rows(isReversed: Boolean = false): Grid =
-    if (!isReversed) this
-    else map(List<Tree>::reversed)
-
-fun Grid.cols(isReversed: Boolean = false): Grid =
-    mapIndexed { y, line ->
-        List(line.size) { x -> this[x][y] }.run {
-            if (!isReversed) this
-            else reversed()
-        }
     }
 
 fun solveFirst(input: List<String>): String =
     input.toGrid()
         .run {
             listOf(
-                rows(),
-                rows(true),
-                cols(),
-                cols(true)
-            ).flatMap { grid -> grid.flatMap(List<Tree>::visibleTrees) }
+                this,
+                this.map(List<Tree>::reversed),
+                rowsToCols(),
+                rowsToCols().map(List<Tree>::reversed)
+            ).flatMap { it.flatMap(List<Tree>::visibleFromGround) }
         }
         .toSet()
         .count()
         .toString()
 
-
-fun List<Tree>.visibleFromTreeHouse(house: Tree): List<Tree> {
-    fun iterate(index: Int, matches: List<Tree>): List<Tree> =
-        if (index > lastIndex) matches
-        else this[index].let { tree ->
-            if (tree.height < house.height) iterate(index + 1, matches + listOf(tree))
-            else matches + listOf(tree)
-        }
-    return iterate(0, emptyList())
-}
-
-fun Grid.scenicScore(tree: Tree): Int {
-    println("${tree.x}, ${tree.y}")
-    // get tree in grid
-    return listOf(
-        rows()[tree.y].run { subList(tree.x, size) },
-        rows()[tree.y].run { subList(0, tree.x + 1).reversed() },
-        cols()[tree.x].run { subList(tree.y, size) },
-        cols()[tree.x].run { subList(0, tree.y + 1).reversed() },
-    ).map { it.drop(1) }
-        .also(::println)
-        .map { it.visibleFromTreeHouse(tree).count() }
-        .also(::println)
-        .reduce { acc, height -> acc * height}
-        .also(::println)
-
-
-
-
-//
-//
-//    val values = listOf(
-//        rows().run { filterIndexed { index, _ -> index == tree.y }.subList(tree.x, size) }.first(),
-//        rows(true).run { filterIndexed { index, _ -> index == tree.y }.subList(0, tree.x) }.first(),
-//        cols().run { filterIndexed { index, _ -> index == tree.x }.subList(tree.x, size) }.first(),
-//        cols(true).run { filterIndexed { index, _ -> index == tree.x }.subList(0, tree.x) }.first()
-//    ).map(List<Tree>::visibleTrees).map(List<Tree>::count).also(::println)
-//
-//    return values.reduce { acc, scenery -> acc * scenery }
-}
-
-fun solveSecond(input: List<String>): String {
-    return input.toGrid()
+fun solveSecond(input: List<String>): String =
+    input.toGrid()
         .run {
             flatMap { trees -> trees.map(::scenicScore) }
         }
         .max()
         .toString()
-}
-
-
-
-
 
 fun main() {
     val pathPrefix = "./src/main/kotlin/year_2022/day_08"
