4b15473e184b0d0da0ae07a71edded9c02b6c3e8
EinMilchBoss <christian.benezuela@gmail.com>
2023-07-04 20:15:41 +0200

    Refactor recursion and sort functions into correct files.

--- src/main/kotlin/year_2022/day_13/packet/Packet.kt
+++ src/main/kotlin/year_2022/day_13/packet/Packet.kt
@@ -1,8 +1,21 @@
 package year_2022.day_13.packet
 
+import year_2022.day_13.packet.pair.PacketPair
+
 data class Packet(val data: List) {
-    sealed class Data
-    data class Value(val value: Int) : Data()
+    sealed class Data {
+        fun toPacketListIfValue(): List =
+            when (this) {
+                is Value -> toPacketList()
+                is List -> this
+            }
+    }
+
+    data class Value(val value: Int) : Data() {
+        fun toPacketList(): List =
+            List(listOf(this))
+    }
+
     data class List(val values: kotlin.collections.List<Data>) : Data() {
         operator fun plus(value: Value): List =
             List(values + value)
@@ -12,11 +25,20 @@ data class Packet(val data: List) {
     }
 
     data class DataPair(val left: Data, val right: Data) {
+        fun toPacketPair(): PacketPair =
+            PacketPair(Packet(left as List), Packet(right as List))
+
+        fun toPacketListPair(): Pair<List, List> =
+            Pair(left.toPacketListIfValue(), right.toPacketListIfValue())
+
         fun areBothLists(): Boolean =
             left is List && right is List
 
         fun areBothValues(): Boolean =
             left is Value && right is Value
+
+        fun tryUnwrapPacketValues(): Pair<Int, Int> =
+            Pair((left as Value).value, (right as Value).value)
     }
 }
 
--- src/main/kotlin/year_2022/day_13/packet/pair/PacketPairComparator.kt
+++ src/main/kotlin/year_2022/day_13/packet/pair/PacketPairComparator.kt
@@ -2,67 +2,35 @@ package year_2022.day_13.packet.pair
 
 import year_2022.day_13.packet.Packet
 import year_2022.day_13.packet.exception.IdenticalPacketsException
+import year_2022.day_13.packet.toPacketDataPair
 
-fun iterateAreBothValues(packetPair: PacketPair, packetDataPairs: List<Packet.DataPair>, index: Int): Boolean =
-    packetDataPairs[index].unwrapPacketDataPairValues()
-        .let { (leftValue, rightValue) ->
+fun PacketPair.isInOrder(): Boolean =
+    compareNextPacketDataPair(toPacketDataPairs(), 0)
+
+private fun PacketPair.compareNextPacketDataPair(
+    packetDataPairs: List<Packet.DataPair>,
+    index: Int,
+): Boolean =
+    try {
+        tryToComparePacketDataPair(packetDataPairs, index)
+    } catch (_: IdenticalPacketsException) {
+        tryIfLeftPacketIsSmaller()
+    }
+
+private fun PacketPair.tryToComparePacketDataPair(
+    packetDataPairs: List<Packet.DataPair>,
+    index: Int,
+): Boolean =
+    packetDataPairs.getOrElse(index) { throw IdenticalPacketsException() }
+        .run {
             when {
-                leftValue < rightValue -> true
-                leftValue > rightValue -> false
-                else -> iterate(packetPair, packetDataPairs, index + 1)
+                areBothValues() -> compareValues(packetDataPairs, index)
+                areBothLists() -> iterateThroughLists(packetDataPairs, index)
+                else -> convertAndIterateThroughLists(packetDataPairs, index)
             }
         }
 
-fun iterateAreBothLists(packetPair: PacketPair, packetDataPairs: List<Packet.DataPair>, index: Int): Boolean =
-    try {
-        val newPacketPair = packetDataPairs[index].toPacketPair()
-        iterate(
-            newPacketPair, newPacketPair
-                .toPacketDataPairs(), 0
-        )
-    } catch (_: IdenticalPacketsException) {
-        try {
-            iterate(packetPair, packetDataPairs, index + 1)
-        } catch (_: IdenticalPacketsException) {
-            packetPair
-                .isLeftPacketSmaller()
-        }
-    }
-
-fun iterateOneOfEach(packetPair: PacketPair, packetDataPairs: List<Packet.DataPair>, index: Int): Boolean {
-    val (left, right) = packetDataPairs[index]
-    val (newLeft, newRight) = listOf(left, right).map(Packet.Data::toPacketListIfValue)
-    return try {
-        val newPacketPair = PacketPair(Packet(newLeft), Packet(newRight))
-        iterate(newPacketPair, newPacketPair.toPacketDataPairs(), 0)
-    } catch (_: IdenticalPacketsException) {
-        try {
-            iterate(packetPair, packetDataPairs, index + 1)
-        } catch (_: IdenticalPacketsException) {
-            packetPair
-                .isLeftPacketSmaller()
-        }
-    }
-}
-
-fun iterate(packetPair: PacketPair, packetDataPairs: List<Packet.DataPair>, index: Int): Boolean {
-    return try {
-        val packetDataPair = packetDataPairs.getOrElse(index) { throw IdenticalPacketsException() }
-        when {
-            packetDataPair.areBothValues() -> iterateAreBothValues(packetPair, packetDataPairs, index)
-            packetDataPair.areBothLists() -> iterateAreBothLists(packetPair, packetDataPairs, index)
-            else -> iterateOneOfEach(packetPair, packetDataPairs, index)
-        }
-    } catch (_: IdenticalPacketsException) {
-        packetPair.isLeftPacketSmaller()
-    }
-}
-
-fun PacketPair.isInOrder(): Boolean {
-    return iterate(this, toPacketDataPairs(), 0)
-}
-
-private fun PacketPair.isLeftPacketSmaller(): Boolean {
+private fun PacketPair.tryIfLeftPacketIsSmaller(): Boolean {
     val leftSize = left.data.values.size
     val rightSize = right.data.values.size
     return when {
@@ -72,17 +40,56 @@ private fun PacketPair.isLeftPacketSmaller(): Boolean {
     }
 }
 
-private fun Packet.DataPair.unwrapPacketDataPairValues(): Pair<Int, Int> =
-    Pair((left as Packet.Value).value, (right as Packet.Value).value)
+private fun PacketPair.compareValues(packetDataPairs: List<Packet.DataPair>, index: Int): Boolean =
+    packetDataPairs[index].tryUnwrapPacketValues()
+        .let { (leftValue, rightValue) ->
+            when {
+                leftValue < rightValue -> true
+                leftValue > rightValue -> false
+                else -> compareNextPacketDataPair(packetDataPairs, index + 1)
+            }
+        }
 
-private fun Packet.DataPair.toPacketPair(): PacketPair =
-    PacketPair(Packet(left as Packet.List), Packet(right as Packet.List))
-
-private fun Packet.Data.toPacketListIfValue(): Packet.List =
-    when (this) {
-        is Packet.Value -> this.toPacketList()
-        is Packet.List -> this
+private fun PacketPair.iterateThroughLists(packetDataPairs: List<Packet.DataPair>, index: Int): Boolean =
+    iterateThroughListsSafely(packetDataPairs, index) {
+        tryIterateThroughLists(packetDataPairs, index)
     }
 
-private fun Packet.Value.toPacketList(): Packet.List =
-    Packet.List(listOf(this))
+private fun PacketPair.convertAndIterateThroughLists(
+    packetDataPairs: List<Packet.DataPair>,
+    index: Int,
+): Boolean =
+    iterateThroughListsSafely(packetDataPairs, index) {
+        tryConvertAndIterateThroughLists(packetDataPairs, index)
+    }
+
+private fun PacketPair.iterateThroughListsSafely(
+    packetDataPairs: List<Packet.DataPair>,
+    index: Int,
+    operation: () -> Boolean,
+): Boolean =
+    try {
+        operation()
+    } catch (_: IdenticalPacketsException) {
+        compareNextPacketDataPair(packetDataPairs, index + 1)
+    }
+
+private fun tryIterateThroughLists(
+    packetDataPairs: List<Packet.DataPair>,
+    index: Int,
+): Boolean =
+    packetDataPairs[index].toPacketPair()
+        .run {
+            compareNextPacketDataPair(toPacketDataPairs(), 0)
+        }
+
+private fun tryConvertAndIterateThroughLists(
+    packetDataPairs: List<Packet.DataPair>,
+    index: Int,
+): Boolean =
+    packetDataPairs[index].toPacketListPair()
+        .toPacketDataPair()
+        .toPacketPair()
+        .run {
+            compareNextPacketDataPair(toPacketDataPairs(), 0)
+        }
