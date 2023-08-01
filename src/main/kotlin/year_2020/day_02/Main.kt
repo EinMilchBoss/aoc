799eb92e7dbf80fc6d81632d4a9799be55f2de2c
EinMilchBoss <christian.benezuela@gmail.com>
2023-08-01 13:22:27 +0200

    Refactor 20/02.

--- src/main/kotlin/year_2020/day_02/Main.kt
+++ src/main/kotlin/year_2020/day_02/Main.kt
@@ -1,38 +1,47 @@
 package year_2020.day_02
 
-import utils.test
-import java.io.File
+import utils.aoc.*
 
 data class PasswordEntry(val first: Int, val second: Int, val char: Char, val password: String)
 
-fun String.toPasswordEntry(): PasswordEntry = this.split(' ').let { lineParts ->
-    val (min, max) = lineParts[0].split('-').map(String::toInt)
-    val char = lineParts[1].first()
-    val password = lineParts[2]
+fun String.toPasswordEntry(): PasswordEntry =
+    this.split(' ')
+        .let { lineParts ->
+            val (min, max) = lineParts[0].split('-')
+                .map(String::toInt)
+            val char = lineParts[1].first()
+            val password = lineParts[2]
 
-    PasswordEntry(min, max, char, password)
-}
+            PasswordEntry(min, max, char, password)
+        }
 
-fun solveFirst(input: List<String>): String = input.map(String::toPasswordEntry).count { entry ->
-    entry.password.count { it == entry.char }.let { charCount ->
-        charCount >= entry.first && charCount <= entry.second
-    }
-}.toString()
+fun String.partOne(): String =
+    lines().map(String::toPasswordEntry)
+        .count { entry ->
+            entry.password.count { it == entry.char }
+                .let { charCount ->
+                    charCount >= entry.first && charCount <= entry.second
+                }
+        }
+        .toString()
 
-fun solveSecond(input: List<String>): String = input.map(String::toPasswordEntry).count { entry ->
-    listOf(
-        entry.password[entry.first - 1], entry.password[entry.second - 1]
-    ).count { it == entry.char } == 1
-}.toString()
+fun String.partTwo(): String =
+    lines().map(String::toPasswordEntry)
+        .count { entry ->
+            listOf(
+                entry.password[entry.first - 1], entry.password[entry.second - 1]
+            ).count { it == entry.char } == 1
+        }
+        .toString()
 
 fun main() {
-    val pathPrefix = "./src/main/kotlin/year_2020/day_02"
+    val inputs = Inputs(Exercise(2020, 2))
+    val one = Part.one(inputs, String::partOne)
+    val two = Part.two(inputs, String::partTwo)
 
-    val exampleInput = File("$pathPrefix/example.txt").readLines()
-    println("First test: ${test(exampleInput, "2", ::solveFirst)}")
-    println("Second test: ${test(exampleInput, "1", ::solveSecond)}")
+    println(one.testProtocol("2"))
+    println(two.testProtocol("1"))
 
-    val input = File("$pathPrefix/input.txt").readLines()
-    println("First result: ${solveFirst(input)}")
-    println("Second result: ${solveSecond(input)}")
+    println("Part 1:\n${one.run()}")
+    println("Part 2:\n${two.run()}")
 }
